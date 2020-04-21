0581b81ad8c9060c8a7176f57aeb5404f5837bbe
Veerjot Bhangu <veerjotbhangu@gmail.com>
2020-04-20 21:53:30 -0400

    Added SCM code and source code

---
+++ src/main/java/com/udemy/devops/DevOpsUdemyApplication.java
@@ -0,0 +1,16 @@
+package com.udemy.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
+public class DevOpsUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsUdemyApplication.class, args);
+	}
+
+}
